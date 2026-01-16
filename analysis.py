36fdd95987d09a246f52b9b2f71c664b6cf92d8f
zihaolin2000 <zlin22@ur.rochester.edu>
2026-01-16 08:39:23 -0600

    Fix a typo in comments

--- analysis.py
+++ analysis.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 from hep_ml.metrics_utils import ks_2samp_weighted
 from matplotlib.gridspec import GridSpec, GridSpecFromSubplotSpec
 from matplotlib.ticker import ScalarFormatter
-from .utilities import normalize_vectors
+from matplotlib.colors import LinearSegmentedColormap
+from .utilities import normalize_vectors, MNEff_evaluate
 from .nuisance_flat_tree import NuisanceFlatTree
 
 def transform_momentum_to_reaction_frame(df : pd.DataFrame, selector_lepton : str = 'leading_muon', particle_names : list = []) -> pd.DataFrame:
@@ -50,8 +51,7 @@ def transform_momentum_to_reaction_frame(df : pd.DataFrame, selector_lepton : st
     transverse_plane_y = - df[[f'{selector_lepton}_px', f'{selector_lepton}_py']].values
     # normalize to get unit y-vector
     transverse_plane_y = normalize_vectors(transverse_plane_y)
-    # x-vector is simply y-vector rotated counterclock wise by 90
-    # degrees
+    # x-vector is simply y-vector rotated clock wise by 90 degrees
     transverse_plane_x = np.array([-transverse_plane_y[:,1],transverse_plane_y[:,0]]).T
     for particle_name in particle_names:
         transverse_P = df[[f'{particle_name}_px', f'{particle_name}_py']].values
@@ -213,10 +213,12 @@ def draw_source_target_distributions_and_ratio(source : pd.DataFrame, target : p
             ax_main.text(1.00, 1.02, f'{chr(ord("a")+idx)}.', transform=ax_main.transAxes,ha='right', va='bottom')
 
         if variable == 'weight':
-            # plot source sample new weights
-            ax_main.hist(new_source_weights, log=True, bins=30, alpha=alpha, color='goldenrod')
+            # plot source sample new weights, normalized
+            ax_main.hist(new_source_weights * len(new_source_weights)/np.sum(new_source_weights),
+                         log=True, bins=30, alpha=alpha, color='goldenrod')
             ax_main.tick_params(which='both', direction='in')
             ax_main.set_xlim(0, None)
+            ax_main.set_ylim(0, None)
             ax_main.set_xlabel(f'new weights')
             ax_main.set_ylabel('counts (log scale)')
             continue
@@ -316,3 +318,293 @@ def draw_source_target_distributions_and_ratio(source : pd.DataFrame, target : p
     fig.subplots_adjust(bottom=bottom_adjust)
     plt.show()
 
+def draw_2Dxsec_and_efficiency(df_genie2=[],df_genie3=[],xybins=(np.linspace(0,0.6,20),np.linspace(0,2,20)),fScale_genie2=0,fScale_genie3=0,
+               Xsec_columns=('dpt','pT_muon'), xylabels=('$\delta p_T \ (\\text{GeV}/c)$','$p^\mu_T \ (\\text{GeV}/c)$')):
+    M2, N2, eff2, xedges, yedges, N2err, M2err, R2err = MNEff_evaluate(df=df_genie2,xybins=xybins,reweight=False,Xsec_columns=Xsec_columns)
+    M2_rwt, N2_rwt, eff2_rwt, _, _, N2rwt_err, M2rwt_err, R2rwt_err = MNEff_evaluate(df=df_genie2, reweight=True,xybins=xybins,Xsec_columns=Xsec_columns)
+    M3, N3, eff3, _, _, N3err, M3err, R3err = MNEff_evaluate(df=df_genie3,xybins=xybins,reweight=False,Xsec_columns=Xsec_columns)
+    datas = [N2, M2, eff2, N3, M3, eff3, N2_rwt, M2_rwt, eff2_rwt]
+    titles = ['$N_{v2}$',
+              '$M_{v2}$',
+              '$\phi_{v2}=M_{v2}/N_{v2}$',
+               '$N_{v3}$', 
+               '$M_{v3}$', 
+               '$\phi_{v3}=M_{v3}/N_{v3}$', 
+               '$N_{v2}\'$', 
+               '$M_{v2}\'$', 
+               '$\phi_{v2}\'=M_{v2}\'/N_{v2}\'$'
+        ]
+    # titles = ['','','','','','','','','']
+    xlabel, ylabel = xylabels
+    fig, axs = plt.subplots(nrows=3, ncols=3, figsize=(11, 9),dpi=300)  # 3x3 grid
+    vmax = 1.9e-38
+    for i, (ax, data, title) in enumerate(zip(axs.flat, datas, titles)):
+        X, Y = np.meshgrid(xedges, yedges)
+        area=(xedges[1]-xedges[0])*(yedges[1]-yedges[0])
+        # cmap = LinearSegmentedColormap.from_list("black_white_red", ['black','white','red'], N=256)
+        cmap = 'viridis'
+        if i in [0,1]:
+            mesh = ax.pcolormesh(X, Y, data.T*fScale_genie2/area, shading='auto', cmap=cmap, vmin=0.0,vmax=vmax)
+
+        elif i in [3,4]:
+            mesh = ax.pcolormesh(X, Y, data.T*fScale_genie3/area, shading='auto', cmap=cmap,vmin=0.0,vmax=vmax)
+        elif i in [6,7]:
+            # mesh = ax.pcolormesh(X, Y, data.T*(len(df_genie3)*fScale_genie3/len(df_genie2))/area, shading='auto', cmap='viridis')  # Transpose counts
+            mesh = ax.pcolormesh(X, Y, data.T/area, shading='auto', cmap=cmap,vmin=0.0,vmax=vmax)
+        else:
+            mesh = ax.pcolormesh(X, Y, data.T, shading='auto', cmap=cmap,vmin=0.0,vmax=1.0) # efficiency plot
+
+        cbar = plt.colorbar(mesh, ax=ax)
+        if i in [0,1,3,4,6,7]:
+            cbar.set_label('$\\frac{d^2\\sigma}{d\delta p_T d p^T_{\mu}} \ \left(\\frac{\\text{cm}^2}{(\\text{GeV}/c)^2}\\right)$')
+        else:
+            cbar.set_label('efficiency')
+
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+        ax.set_title(f'{chr(ord("a")+i)}. '+title)
+        ax.set_yticks(yedges)
+        ax.set_yticklabels([f"{edge:.2f}" for edge in yedges])
+
+        # plt.show()
+    plt.tight_layout()
+    # plt.show()
+    
+    
+    
+
+        
+    datas = [eff2/eff3,eff2_rwt/eff3]
+    titles = ['Ratio $\phi_{v2}/\phi_{v3}$','Ratio $\phi_{v2}\'/\phi_{v3}$']
+
+    # vmin,vmax=(0.0, np.max([datas[0],datas[1]]))
+    # vmin,vmax=(0.0, 3.0)
+    vmin,vmax=(0.0, 2.0)
+
+
+    print('vmin, vmax:',vmin,vmax)
+
+        
+
+    dpt_bin_centers = 0.5 * (xedges[:-1] + xedges[1:])
+    # print('dpt bincenters:',dpt_bin_centers)
+    eff23_ratio = eff2/eff3
+    ratio23_err = eff23_ratio * np.sqrt((R2err/eff2)**2+(R3err/eff3)**2)
+
+    
+    # eff2_rwt/eff3 histogram slices
+    eff2rwt3_ratio = eff2_rwt/eff3
+    ratio2rwt3_err = eff2rwt3_ratio * np.sqrt((R2rwt_err/eff2_rwt)**2+(R3err/eff3)**2)
+
+
+
+    # new: 2025 July 25 ______________________________________________________________________________________________________
+    fig = plt.figure(figsize=(10, 8.5), dpi=300)
+    outer = GridSpec(2, 2, figure=fig, hspace=0.3, wspace=0.07)
+
+    # Top-left
+    ax00 = fig.add_subplot(outer[0, 0])
+
+    data=eff2/eff3
+    X, Y = np.meshgrid(xedges, yedges)
+    cmap = LinearSegmentedColormap.from_list("black_white_red", ['black','white','red'], N=256)
+    # cmap = 'viridis'
+
+    mesh = ax00.pcolormesh(X, Y, data.T, vmin=vmin, vmax=vmax, shading='auto', cmap=cmap)  # Transpose counts
+    cbar = plt.colorbar(mesh, ax=ax00)
+    cbar.set_ticks(np.linspace(vmin, vmax, 11))  # optional: set colorbar ticks at 0.0, 0.1, ..., 1.0
+    # cbar.set_label('ratio')
+    ax00.set_xlabel(xlabel)
+    ax00.set_ylabel(ylabel)
+    ax00.set_title(
+        'a.',
+        # 'a. Ratio $\phi_{v2}/\phi_{v3}$',
+        fontsize=11)
+    ax00.set_yticks(yedges)
+    ax00.set_yticklabels([f"{edge:.2f}" for edge in yedges])
+
+    # Top-right
+    ax01 = fig.add_subplot(outer[0, 1])
+    data=eff2_rwt/eff3
+    X, Y = np.meshgrid(xedges, yedges)
+    mesh = ax01.pcolormesh(X, Y, data.T, vmin=vmin, vmax=vmax, shading='auto', cmap=cmap)  # Transpose counts
+    cbar = plt.colorbar(mesh, ax=ax01)
+    cbar.set_ticks(np.linspace(vmin, vmax, 11))  # optional: set colorbar ticks at 0.0, 0.1, ..., 1.0
+    # cbar.set_label('ratio')
+    ax01.set_xlabel(xlabel)
+    ax01.set_ylabel(ylabel)
+    ax01.set_title(
+        'b.',
+        # 'b. Ratio $\phi_{v2}\'/\phi_{v3}$',
+        fontsize=11)
+    ax01.set_yticks(yedges)
+    ax01.set_yticklabels([f"{edge:.2f}" for edge in yedges])
+
+
+
+    half = eff2rwt3_ratio.shape[1]//2
+
+    # Bottom-left: nested grid 
+    inner = GridSpecFromSubplotSpec(eff2rwt3_ratio.shape[1]-half, 1, subplot_spec=outer[1, 0], hspace=0.05)
+    for index in range(half,eff2rwt3_ratio.shape[1]):
+        ax = fig.add_subplot(inner[index-half])
+        i = eff2rwt3_ratio.shape[1]-1-index
+
+        # draw a line at y=1
+        ax.axhline(y=1, color='black', linestyle='--',alpha=0.5)
+        # draw a invisible line to label "c." for paper...
+        ax.axhline(y=0, color='black', linestyle='-',alpha=0.0,label='c.')
+
+
+        # eff2_rwt/eff3
+        slice_counts = eff2rwt3_ratio.T[i, :]
+        errors = ratio2rwt3_err.T[i,:]
+
+        slice_counts[np.isnan(slice_counts)] = 0
+        ax.step(xedges, np.append(slice_counts, slice_counts[-1]), where='post',label='$\phi_{v2}\'/\phi_{v3}$',color='purple',alpha=0.7)
+        ax.errorbar(dpt_bin_centers - 0.1*np.diff(xedges), slice_counts, yerr=errors, capsize = 2,fmt='none',color='purple',markersize=2,alpha=0.7)
+
+        yge2 = np.any((slice_counts+errors) >= 2)
+
+        # eff2/eff3
+        slice_counts = eff23_ratio.T[i, :]
+        errors = ratio23_err.T[i,:]
+        #fill nan with zero:
+        slice_counts[np.isnan(slice_counts)] = 0
+        ax.step(xedges, np.append(slice_counts, slice_counts[-1]), where='post',label='$\phi_{v2}/\phi_{v3}$',color='orange',alpha=0.7)
+        ax.errorbar(dpt_bin_centers, slice_counts, yerr=errors,  markersize=2,capsize = 2,fmt='none',color='orange',alpha=0.7)
+
+
+        # add a text line at top left
+        ax.text(0.95, 0.9, f'{round(yedges[i],2)} $\le ~ p^T_\mu ~ <$ {round(yedges[i+1],2)} (GeV/$c$)', fontsize=8,
+                horizontalalignment='right',verticalalignment='top', transform=ax.transAxes)
+
+        ax.set_xlim(0,xedges[-1])
+        
+        # yge2 = yge2 or np.any((slice_counts+errors) >= 1.3)
+        # if yge2:
+        #     ax.set_ylim(0.7,None)
+        # else:
+        #     ax.set_ylim(0.7,1.3)
+        points = slice_counts+errors
+        points[~np.isfinite(points)] = 1.0
+        ymax = np.max(points-1)*1.1
+
+        # yrange = max(np.max(points-1),np.max(1-points))*5
+        # ax.set_ylim(1-yrange,1+yrange)
+        points = slice_counts-errors
+        points[~np.isfinite(points)] = 1.0
+        ymin = np.max(1-points)*1.1
+
+        yrange = max(ymin,ymax)
+        ax.set_ylim(1-yrange,1+ yrange)
+
+        # ax.legend()
+        
+        if index < eff2rwt3_ratio.shape[1] - 1:
+            ax.set_xticklabels([])
+
+        if index == eff2rwt3_ratio.shape[1]-1:
+            ax.set_xlabel(xlabel)
+
+        # ax.set_yticks([0,1])
+        # ax.set_yticklabels([0,1])
+
+
+
+        if index == half:
+            ax.legend(loc='lower center', bbox_to_anchor=(1.1, 1.3), ncol=3, frameon=False,fontsize=11)
+        
+        bbox = outer[1, 0].get_position(fig)
+        ax.set_position([
+            bbox.x0,
+            ax.get_position().y0,
+            bbox.width*0.8,
+            ax.get_position().height
+        ])
+
+    # Bottom-right: nested grid 
+    inner = GridSpecFromSubplotSpec(half, 1, subplot_spec=outer[1, 1], hspace=0.05)
+    for index in range(0,half):
+        ax = fig.add_subplot(inner[index])
+        i = eff2rwt3_ratio.shape[1]-1-index
+
+        # draw a line at y=1
+        ax.axhline(y=1, color='black', linestyle='--',alpha=0.5)
+        # draw a invisible line to label "c." for paper...
+        ax.axhline(y=0, color='black', linestyle='-',alpha=0.0,label='c.')
+
+
+        # eff2_rwt/eff3
+        slice_counts = eff2rwt3_ratio.T[i, :]
+        errors = ratio2rwt3_err.T[i,:]
+
+        slice_counts[np.isnan(slice_counts)] = 0
+        ax.step(xedges, np.append(slice_counts, slice_counts[-1]), where='post',label='$\phi_{v2}\'/\phi_{v3}$',color='purple',alpha=0.7)
+        ax.errorbar(dpt_bin_centers - 0.1*np.diff(xedges), slice_counts, yerr=errors, capsize = 2,fmt='none',color='purple',markersize=2,alpha=0.7)
+
+        yge2 = np.any((slice_counts+errors) >= 2)
+
+        # eff2/eff3
+        slice_counts = eff23_ratio.T[i, :]
+        errors = ratio23_err.T[i,:]
+        #fill nan with zero:
+        slice_counts[np.isnan(slice_counts)] = 0
+        ax.step(xedges, np.append(slice_counts, slice_counts[-1]), where='post',label='$\phi_{v2}/\phi_{v3}$',color='orange',alpha=0.7)
+        ax.errorbar(dpt_bin_centers, slice_counts, yerr=errors,  markersize=2,capsize = 2,fmt='none',color='orange',alpha=0.7)
+
+
+        # add a text line at top right
+        ax.text(0.05, 0.9, f'{round(yedges[i],2)} $\le ~ p^T_\mu ~ <$ {round(yedges[i+1],2)} (GeV/$c$)', fontsize=8,
+                horizontalalignment='left',verticalalignment='top', transform=ax.transAxes)
+
+        ax.set_xlim(0,xedges[-1])
+        
+        # yge2 = yge2 or np.any((slice_counts+errors) >= 1.3)
+        # if yge2:
+        #     ax.set_ylim(0.7,None)
+        # else:
+        #     ax.set_ylim(0.7,1.3)
+        points = slice_counts+errors
+        points[~np.isfinite(points)] = 1.0
+        ymax = np.max(points-1)*2
+
+        yrange = max(np.max(points-1),np.max(1-points))*1.1
+        # ax.set_ylim(1-yrange,1+yrange)
+        points = slice_counts-errors
+        points[~np.isfinite(points)] = 1.0
+        ymin = np.max(1-points)*1.1
+
+        yrange = max(ymin,ymax)
+        ax.set_ylim(1-yrange,1+ yrange)        
+        
+        
+        # ax.legend()
+        
+        if index < half - 1:
+            ax.set_xticklabels([])
+
+        if index == half-1:
+            ax.set_xlabel(xlabel)
+
+        # ax.set_yticks([0,1])
+        # ax.set_yticklabels([0,1])
+
+
+
+        # if index == 0:
+        #     ax.legend(loc='lower center', bbox_to_anchor=(0.5, 1.0), ncol=3, 
+        #             #   fontsize=20, 
+        #               frameon=False)
+        
+        bbox = outer[1, 1].get_position(fig)
+        ax.set_position([
+            bbox.x0,
+            ax.get_position().y0,
+            bbox.width*0.8,
+            ax.get_position().height
+        ])
+
+
+
+    plt.show()
