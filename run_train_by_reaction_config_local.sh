66a2545a9caf021f6a03e3172376063f7ab10d5a
lgiannes <lorenzo.giannessi@unige.ch>
2026-07-13 15:38:40 +0200

    now user-set training parameters through yaml config

---
+++ run_train_by_reaction_config_local.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+source="/Users/lorenzo/cernbox/MINERVA_MC/source//ReweightSourceCCQELike_ABCDEFGLMNOP.root"
+# source="/Users/lorenzo/cernbox/MINERVA_MC/source//ReweightSourceCCQELike_minervame1G_50_10.root"
+
+ target="/Users/lorenzo/cernbox/MINERVA_MC/target/neut_MINERvAflux_SF_nu_all_NUISFLAT_CCQELike.root"
+# target="/Users/lorenzo/cernbox/MINERVA_MC/target/neut_MINERvAflux_EDRMF_nu_all_NUISFLAT_CCQELike.root"
+# target="/Users/lorenzo/cernbox/MINERVA_MC/target/flat_NuWro_CH_LFG_v2109_50M_CCQELike.root"
+#target="/Users/lorenzo/cernbox/MINERVA_MC/target/flat_GENIE_G18_10b_02_11a_50M_CCQELike.root"
+
+ target_model_id="NEUT-SF"
+#target_model_id="GENIEv3"
+#target_model_id="NuWro-LFG"
+#target_model_id="TEST-EDRMF"
+
+module_path="/Users/lorenzo/Minerva/fork_reweighting/"
+config="${module_path}/BDTReweight/train_by_reaction_config.yaml"
+
+PYTHONPATH=${module_path}:${PYTHONPATH}
+
+python3 ${module_path}/BDTReweight/train_by_reaction_config.py \
+                    --config $config \
+                    --source_path $source \
+                    --target_path $target \
+                    --module_path $module_path \
+                    --model_name ${target_model_id}
+#                    --max_events 300000
+                    # --category 0p0ns
